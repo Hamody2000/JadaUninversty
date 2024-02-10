a81fbd8af56243f3dfe473151b8daec6f5d46f1f
Hamody <ahmed.hamody5532@gmail.com>
2024-02-10 15:32:24 +0200

    autocomplete 'off'

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -11,12 +11,12 @@
                     {{ ___('common.Please confirm your password before continuing.') }}
 
                     <form method="POST" action="{{ route('password.confirm') }}">
-                        @csrf 
+                        @csrf
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ ___('common.Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="off">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
